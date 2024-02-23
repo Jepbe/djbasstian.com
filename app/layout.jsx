db3eba5cf23dcfc0d0113f7df76058b1d82b028b
Jeppe <jepbe123@gmail.com>
2024-02-23 13:42:58 +0100

    favicon test

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,20 @@
 import './ui/globals.css';
 import { Ubuntu } from 'next/font/google'
 
-
 const ubuntu = Ubuntu({
   weight: '500',
   subsets: ['latin'],
 })
+
+
+export const metadata = {
+  title: "DJ BASStian - Dj til din næste fest!",
+  description: "DJ BASStian DJ bastian DJBASSTIAN",
+  icons: {
+    icon: '../public/favicon.ico'
+  }  
+};
+
 
 export default function DashboardLayout({ children }) {
 
