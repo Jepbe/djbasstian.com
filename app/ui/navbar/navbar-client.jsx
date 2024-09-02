b69ee50c05184e32b2a3a136a0e1fda5a8418b4f
Jeppe Bøgeskov <jepbe123@gmail.com>
2024-09-02 12:20:15 +0200

    notify removed

--- app/ui/navbar/navbar-client.jsx
+++ app/ui/navbar/navbar-client.jsx
@@ -5,14 +5,11 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import bassLogo from '../../../public/bass_logo_1_hvid.png'
 import { FaInstagram, FaSoundcloud, FaYoutube, FaTiktok } from 'react-icons/fa';
-import { IoClose } from "react-icons/io5";
 
 
 export default function NavbarClient({ children }) {
-    
     const [navbar, setNavbar] = useState( false );
     const [color, setColor] = useState( false );
-    const [notification, setNotification] = useState( false );
 
     useEffect(() => {
         const changeBackground = () => {
@@ -28,7 +25,7 @@ export default function NavbarClient({ children }) {
 
     return (
     <> 
-        <button onClick={() => setNavbar(!navbar)} className={`z-50 cursor-pointer md:hidden fixed right-7 ${notification ? 'top-[55px]' : 'top-[90px]'} text-white`}>
+        <button onClick={() => setNavbar(!navbar)} className={`z-50 cursor-pointer md:hidden fixed right-7 top-[55px] text-white`}>
             {navbar ? 
             <span className="relative bottom-[10px] left-[6px]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
@@ -45,23 +42,9 @@ export default function NavbarClient({ children }) {
         </button>
 
         <header className={`${color ? 'bg-gray-400' : 'bg-transparent'} fixed  left-0 w-full z-20 drop-shadow-lg`}>
-        
-        {notification ? 
-            <meta />
-        :
-            <div className="w-full h-16  sm:h-10 bg-yellow-400 flex justify-center flex-row items-center z-40">
-                <div className="absolute top-[23px] sm:top-[10px] left-3 sm:left-3">
-                    <button onClick={() => setNotification(!notification)} ><i><IoClose/></i></button>
-                </div>
-                <h1 className=" pl-10 sm:pl-0">Køb din billet til Beachparty Faxe Ladeplads her: </h1>
-                <Link href="https://billetto.dk/e/beachparty-2024-billetter-1017370">
-                    <button className="mr-2 sm:mr-0 sm:ml-2 px-2 p-[1px] text-white bg-[#436cff] cursor-pointer rounded-md font-medium">Køb billetter</button>
-                </Link>
-            </div>
-        }
 
             <nav className={`text-white flex  justify-between m-0 ${navbar ? 'flex-col' :'flex-row'}`}>
-                <ul className={`flex flex-row ${notification ? 'mt-4' : 'mt-[-10px]'} md:mt-0`}>
+                <ul className={`flex flex-row mt-4 md:mt-0`}>
                     <Link href="/">
                         <span>
                             <Image itemProp="logo" src={bassLogo} width={140} height={100} alt="DJ BASSTIAN Dj Basstian Logo djbasstian-logo" />                        
